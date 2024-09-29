fa423431a5dae5c6ed6bac710d1449e9a8c16334
Ashlc <mibp@ic.ufal.br>
2024-09-28 21:17:37 -0300

    :lipstick: feat: Improving navigation and button logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { FileOpen } from '@mui/icons-material';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { Button, Stack } from '@mui/material';
-import Box from '@mui/material/Box';
+import { Box, Button } from '@mui/material';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import { SyntheticEvent, useState } from 'react';
@@ -12,12 +11,11 @@ import Doctor from './pages/Doctor/Doctor';
 import External from './pages/External/External';
 import Identification from './pages/Identification/Identification';
 import Infant from './pages/Infant/Infant';
-import Locality from './pages/Locality/Locality';
 import Occurence from './pages/Occurence/Occurence';
 import { isInfant } from './utils/handleAge';
 
 function App() {
-  const [activeTab, setActiveTab] = useState(3);
+  const [activeTab, setActiveTab] = useState(0);
   const methods = useForm();
   const [birth, death] = methods.watch(['dateOfBirth', 'dateOfDeath']);
   const typeOfDeath = methods.watch('typeOfDeath');
@@ -33,7 +31,7 @@ function App() {
   const tabUp = () => {
     if (activeTab + 1 === 2 && infantDisabled)
       return setActiveTab((prev) => prev + 2);
-    return activeTab === 6 ? null : setActiveTab((prev) => prev + 1);
+    return activeTab === 5 ? null : setActiveTab((prev) => prev + 1);
   };
 
   const tabDown = () => {
@@ -55,13 +53,26 @@ function App() {
               className="w-full"
             >
               <Tabs value={activeTab} onChange={handleChange} centered>
+                <Button
+                  onClick={tabDown}
+                  disabled={activeTab === 0}
+                  variant="text"
+                >
+                  <NavigateBeforeIcon />
+                </Button>
                 <Tab label="Identificação" value={0} />
                 <Tab label="Ocorrência" value={1} />
                 <Tab label="Infantil" value={2} disabled={infantDisabled} />
                 <Tab label="Condições e causas" value={3} />
                 <Tab label="Médico" value={4} />
                 <Tab label="Causas externas" value={5} />
-                <Tab label="Localidade s/ Médico" value={6} />
+                <Button
+                  onClick={tabUp}
+                  disabled={activeTab === 5}
+                  variant="text"
+                >
+                  <NavigateNextIcon />
+                </Button>
               </Tabs>
             </Box>
             <Identification index={0} value={activeTab} />
@@ -70,39 +81,18 @@ function App() {
             <Conditions index={3} value={activeTab} />
             <Doctor index={4} value={activeTab} />
             <External index={5} value={activeTab} />
-            <Locality index={6} value={activeTab} />
           </form>
-          <Stack
-            direction={'row'}
-            spacing={2}
-            className="w-full justify-between"
-          >
+          {activeTab === 5 && (
             <Button
-              onClick={methods.handleSubmit(onSubmit)}
+              type="submit"
+              className="mt-4"
               variant="contained"
-              startIcon={<FileOpen />}
+              size="large"
+              endIcon={<FileOpen />}
             >
               Gerar D.O.
             </Button>
-            <Stack direction={'row'} spacing={2}>
-              <Button
-                onClick={tabDown}
-                disabled={activeTab === 0}
-                variant="contained"
-                startIcon={<NavigateBeforeIcon />}
-              >
-                Voltar
-              </Button>
-              <Button
-                onClick={tabUp}
-                disabled={activeTab === 6}
-                variant="contained"
-                endIcon={<NavigateNextIcon />}
-              >
-                Avançar
-              </Button>
-            </Stack>
-          </Stack>
+          )}
         </FormProvider>
       </div>
     </div>
